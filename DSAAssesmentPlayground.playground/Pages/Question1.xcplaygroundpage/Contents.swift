4f8d388c0d9f5415d283ca478a4fccfbf7737089
Tiffany Obi <tiffanyobi123@gmail.com>
2020-03-30 10:52:38 -0400

    added playground

---
+++ DSAAssesmentPlayground.playground/Pages/Question1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var str = "Hello, playground"
+// Q1.
+// Given a linked list, write a function to traverse it and print each value in order.
+class Node<T> {
+    var next: Node?
+    let val: T
+    init(val: T) {
+        self.val = val
+    }
+}
+
+//// Print format:
+//```
+//-> 4 -> 9 -> 2 -> 3 -> nil
+//```
